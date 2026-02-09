f292aa1e472b329aebf501657d7becbf1b03614a
adribonns <adrien.bonneu@liebherr.com>
2026-02-09 16:50:08 +0100

    add mkdocs

--- rago/eval/__init__.py
+++ rago/eval/__init__.py
@@ -5,6 +5,9 @@ from rago.eval.sim_score import SimilarityScore
 from rago.eval.llm_evaluator import BaseLLMEvaluator, CoTLLMEvaluator, SimpleLLMEvaluator, PolicyOnError, JudgeError, EvalPrompts
 from rago.eval.bert_score import BertScore
 
+# Import modules to trigger @register_evaluator decorators
+from rago.eval import bert_score, sim_score  # noqa: F401
+
 __all__ = [
     "BaseDependentEvaluator",
     "BaseEvaluator",
